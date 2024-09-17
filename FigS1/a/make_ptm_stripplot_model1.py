90d7f8eb05f58b728f3b75074dae91a91b3909c1
porterll <75682642+porterll@users.noreply.github.com>
2024-09-17 10:19:13 -0400

    Add files via upload

--- FigS1/a/make_ptm_stripplot_model1.py
+++ FigS1/a/make_ptm_stripplot_model1.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     f = open('RfaH_Colabfold_MSA_plddts.txt').read().splitlines()
     f2 = open('good_RFAH_autoinhibited_plDDTS_curated.txt').read().splitlines()
-    f3 = open('RfaH_AF2.2_ptm_nomaskx250.txt').read().splitlines()
+    f3 = open('RfaH_colabfold_msax250_model1_ptm_plddts_sorted.txt').read().splitlines()
 
     nums = []
     nums3 = []
@@ -40,21 +40,23 @@ if __name__ == '__main__':
         info = i.split()
         nums3.append(float(info[1][:-1]))
         
-    print(min(nums))
+    #print(min(nums))
     
 
     dict_info = {'Whole MSA':nums,'Whole MSA, model 1': nums3, 'AF-cluster':nums2}
+    #dict_info = {'Whole MSA': nums3, 'AF-cluster':nums2}
 
     labels = []
     for i in range(len(nums)):
         labels.append('Whole MSA\nmonomer')
     for i in range(len(nums3)):
-        labels.append('Whole MSA\nptm, model 1')
+        labels.append('Whole MSA')
     for i in range(len(nums2)):
         labels.append('AF-cluster')
 
 
     all_nums = nums+nums3+nums2
+    #all_nums = nums3+nums2
 
     dat = {'Method':labels,'pLDDT':all_nums}
 
@@ -62,28 +64,28 @@ if __name__ == '__main__':
     ax = fig.add_subplot(111)
 
     sns.stripplot(data=dat,x='Method',y='pLDDT',zorder=0)
-    plt.plot([0.85,1.15],[68.6,68.6],'r-',zorder=10)
-    plt.plot([1.85,2.15],[73.9,73.9],'r-',zorder=10)
-    plt.setp(ax.get_xticklabels(), ha="right", rotation=45)
 
 
     avg = np.average(nums)
-    med = np.median(nums)
+    #med = np.median(nums)
 
     avg2=np.average(nums3)
 
     avg3 = np.average(nums2)
 
     
-    print(avg)
+    #print(avg)
     print(avg2)
     print(avg3)
 
     plt.plot([-0.15,0.15],[avg,avg],'k-',zorder=10)
     plt.plot([0.85,1.15],[avg2,avg2],'k-',zorder=10)
+    #plt.plot([-0.15,0.15],[68.6,68.6],'r-',zorder=10)
+    #plt.plot([0.85,1.15],[73.9,73.9],'r-',zorder=10)
+    plt.setp(ax.get_xticklabels(), ha="right", rotation=45)
     plt.plot([1.85,2.15],[avg3,avg3],'k-',zorder=10)
     plt.ylabel('plDDT')
 
     plt.tight_layout()
 
-    plt.savefig('ptm_plot_all.png',dpi=1200)
+    plt.savefig('ptm_comparison_model1.png',dpi=1200)
